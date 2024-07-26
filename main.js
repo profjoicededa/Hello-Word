8238a24e8665fe2700263b821f3cab03a87e7ad8
profjoicededa <joicesa@prof.educacao.sp.gov.br>
2024-07-25 21:25:08 -0300

    Construção do Main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let nomeUsuário =prompt("Olá mundo");
+let elemento = document.querySelector("#nome-usuário");
+
+while (nomeUsuário == "") {
+   nomeUsuário = prompt("Qual o seu nome?");
+}
+
+if(nomeUsuário == null){
+    elemento.textContent = "Seja muito bem-vindo";
+}else{
+    elemento.textContent = nomeUsuário;
+}
+
